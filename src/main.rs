e8e6c95a9edcc29f44986806f10736041f8682fd
Stefan-Schroder <stefans040297@outlook.com>
2024-08-21 15:06:22 +0200

    made logical changes to maps (index 0), used some and my code looks so much hottttter

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{BufReader, BufRead};
 
 use clap::Parser;
 
+use std::char;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -12,97 +14,86 @@ struct Args {
 
 #[derive(Debug)]
 struct Node {
-    variant: u8,
-    index: i32,
-    children: Vec<Node>,
+    index: Option<String>,
+    children: [Option<Box<Node>>; 3],
 }
 
 impl Node {
     fn new(v: u8)->Self {
         Node {
-            variant: v,
-            index: -1,
-            children: Vec::new(),
+            index: None,
+            children: [None, None, None],
         }
     }
 
-    fn set_index(&mut self, i: i32) {
-        self.index = i;
+    fn set_index(&mut self, i: String) {
+        self.index = Some(i);
     }
 
-    fn get_index(&self)->i32 {
-        self.index
+    fn get_index(&self)->& String {
+        self.index.as_ref().expect("You need to check first you hoe")
     }
 
     fn is_end(& self)->bool {
-        self.index != -1
+        self.index.is_some()
     }
 
     fn insert_and_return(&mut self, v: u8)-> &mut Node {
-        let mut return_index = 0;
-        let mut found = false;
+        match &self.children[v as usize]{
+            Some(_s) => {
+            },
+            None => {
+                self.children[v as usize] = Some(Box::new(Node::new(v)));
+            },
+        };
 
-        for (i, c) in self.children.iter_mut().enumerate() {
-            if c.variant == v {
-                return_index = i;
-                found = true;
-                break;
-            }
-        }
-
-        if !found {
-            self.children.push(Node::new(v));
-            return_index = self.children.len() - 1;
-        }
-
-        return &mut self.children[return_index];
+        return self.children[v as usize].as_mut().unwrap();
     }
 
     fn mermaid_print(&self, s: &mut String) {
         if self.is_end() {
-            s.push((self.variant + '0' as u8) as char);
-            println!("{} --> i{}([{}])", s, self.index, translate(s)); 
-            s.pop();
-        }
-        else {
-            for c in self.children.iter() {
-                println!("{}{} --> {}{}{}[{}]", s, self.variant, s, self.variant, c.variant, c.variant);
-
-                s.push((self.variant + '0' as u8) as char);
-                c.mermaid_print(s);
+            println!("{} --> i{}({:?})", s, s, self.index.as_ref().expect("Bish i checked this"));
+        } else {
+            for (i, c) in self.children.iter().enumerate() {
+                if c.is_none() {
+                    continue;
+                }
+                println!("{} --> {}{}[{}]", s, s, i, i + 1);
+                s.push((i as u8 + b'0') as char);
+                c.as_ref().expect("If this crashes i am dumb").mermaid_print(s);
                 s.pop();
             }
         }
     }
 
-    fn solve(&self, parents: Vec<& Node>) {
-        let mut space = [0, 0, 0];
-
-        for parent in parents.iter() {
-            for child in parent.children.iter() {
-                space[child.variant as usize - 1] += 1;
-            }
-        }
-
-        // the 4th one is for a * space
-        let mut explore = [false,false,false,true];
-
-        for (i,x) in space.iter().enumerate() {
-            if space[i] == parents.len() { // all parents have this in their space
-                explore[i] = true;
-            }
-            else {
-                explore[3] = false; // one space is not present so the all space is not possible
-            }
-        }
-
-        for (i,x) in explore.iter().enumerate().rev() {
-        }
-
-        println!("{:?}", space);
-        println!("{:?}", explore);
-
-    }
+    //fn solve(&self, parents: Vec<& Node>) {
+    //    let mut space = [0, 0, 0];
+    //
+    //    for parent in parents.iter() {
+    //        for child in parent.children.iter() {
+    //            space[child.variant as usize - 1] += 1;
+    //        }
+    //    }
+    //
+    //    // the 4th one is for a * space
+    //    let mut explore = [false,false,false,true];
+    //
+    //    for (i,x) in space.iter().enumerate() {
+    //        if space[i] == parents.len() { // all parents have this in their space
+    //            explore[i] = true;
+    //        }
+    //        else {
+    //            explore[3] = false; // one space is not present so the all space is not possible
+    //        }
+    //    }
+    //
+    //    for (i,x) in explore.iter().enumerate().rev() {
+    //    }
+    //
+    //    println!("{:?}", space);
+    //    println!("{:?}", explore);
+    //
+    //}
 
 }
 
@@ -113,15 +104,15 @@ fn translate(s: &String)->String{
     let number = ["one", "two", "three"];
 
     let mut ret = String::new();
-    ret.push_str(number[s.chars().nth(1).unwrap() as usize - '0' as usize - 1]);
+    ret.push_str(number[s.chars().nth(0).unwrap() as usize - '0' as usize]);
     ret.push_str(" ");
-    ret.push_str(colour[s.chars().nth(2).unwrap() as usize - '0' as usize - 1]);
+    ret.push_str(colour[s.chars().nth(1).unwrap() as usize - '0' as usize]);
     ret.push_str(" ");
-    ret.push_str(shade[s.chars().nth(3).unwrap() as usize - '0' as usize - 1]);
+    ret.push_str(shade[s.chars().nth(2).unwrap() as usize - '0' as usize]);
     ret.push_str(" ");
-    ret.push_str(shape[s.chars().nth(4).unwrap() as usize - '0' as usize - 1]);
+    ret.push_str(shape[s.chars().nth(3).unwrap() as usize - '0' as usize]);
     
-    if s.chars().nth(4).unwrap() as usize - '0' as usize > 1 {
+    if s.chars().nth(3).unwrap() as usize - '0' as usize > 0 {
         ret.push_str("s");
     } else { 
         ret.push_str("");
@@ -147,14 +138,14 @@ fn main() -> std::io::Result<()> {
         for c in line.chars() {
             current_node = current_node.insert_and_return(c as u8 - '0' as u8);
         }
-        current_node.set_index(i as i32);
+        current_node.set_index(translate(&line));
     }
 
-    head.mermaid_print(&mut "".to_string());
+    head.mermaid_print(&mut "x".to_string());
 
-    println!("Solving...");
-    let root_vec: Vec<& Node> = vec![& head];
-    head.solve(root_vec);
+    //println!("Solving...");
+    //let root_vec: Vec<& Node> = vec![& head];
+    //head.solve(root_vec);
 
     Ok(())
 }
